ae19f6ff50077f4b9fd2a13d03e355eea62bbef2
firefreezing <firefreezing@gmail.com>
2015-02-21 19:28:17 -0500

    Programming Assignment 2-update: adding new comments

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,17 @@
+# catcheSolve function reads in another function defined as makeCacheMatrix. It loads
+# in the inverse of the square matrix if it has been calculated and cached before, or it 
+# calculates the inverse should it is the first time seeing the matrix.
+
 cacheSolve <- function(x, ...) {
-  m <- x$getMatInv()
+  m <- x$getMatInv()  # If the inverse of the matrix has already been calculated before,
+                      # just load in and signaled "getting cached data" 
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setMatInv(m)
+  data <- x$get()   # Load the square matrix that is saved in makeCacheMatrix$getMatInv.
+  m <- solve(data, ...)   # Compute its inverse.
+  x$setMatInv(m)    # Store the newly solved inverse into function makeCacheMatrix$setMathInv(). 
   m
 }
+
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,14 @@
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y   # Define the value of x, where x is from the parent environment. 
+              # This is the use of <<-.
     m <<- NULL
   }
   get <- function() x
-  setMatInv <- function(inverse) m <<- inverse
-  getMatInv <- function() m
+  setMatInv <- function(inverse) m <<- inverse    # Store the value of inverse into m.
+  getMatInv <- function() m   # The default value of makeCacheMatrix$getMatInv is NULL.
+                              # Its value gets updated by makeCacheMatrix$setMatInv.
   list(set = set, 
        get = get,
        setMatInv = setMatInv,
